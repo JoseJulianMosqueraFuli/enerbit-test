10f4f1ca35b575c9dbbc3b41ccd85296c56a34cf
Jose Mosquera <jojumofu@gmail.com>
2023-10-27 18:46:51 -0500

    fix: typos

--- models/customer.py
+++ models/customer.py
@@ -4,11 +4,12 @@ from sqlalchemy import Column, String, DateTime, Boolean
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 
-from  datetime import datetime
+from datetime import datetime
 from database import Base
 
+
 class Customer(Base):
-    __tablename__ = 'customers'
+    __tablename__ = "customers"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     first_name = Column(String, nullable=False)
@@ -18,12 +19,19 @@ class Customer(Base):
     end_date = Column(DateTime, nullable=True)
     is_active = Column(Boolean, nullable=False, default=False)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
-    
-    work_orders = relationship('WorkOrder', back_populates='owner')
 
+    work_orders = relationship("WorkOrder", back_populates="owner")
 
-
-    def __init__(self, id, first_name, last_name, address, start_date=None, end_date=None, is_active=False):
+    def __init__(
+        self,
+        id,
+        first_name,
+        last_name,
+        address,
+        start_date=None,
+        end_date=None,
+        is_active=False,
+    ):
         self.id = id
         self.first_name = first_name
         self.last_name = last_name
@@ -31,4 +39,3 @@ class Customer(Base):
         self.start_date = start_date
         self.end_date = end_date
         self.is_active = is_active
-        
--- models/work_order.py
+++ models/work_order.py
@@ -4,23 +4,26 @@ from sqlalchemy import Column, String, DateTime, Enum, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 
-from  datetime import datetime
+from datetime import datetime
 from database import Base
 
+
 class WorkOrder(Base):
-    __tablename__ = 'work_orders'
+    __tablename__ = "work_orders"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    customer_id = Column(UUID(as_uuid=True), ForeignKey('customers.id'), nullable=False)
+    customer_id = Column(UUID(as_uuid=True), ForeignKey("customers.id"), nullable=False)
     title = Column(String, nullable=False)
     planned_date_begin = Column(DateTime, nullable=True)
     planned_date_end = Column(DateTime, nullable=True)
-    status = Column(Enum('new', 'done', 'cancelled', name='status_enum'))
+    status = Column(Enum("new", "done", "cancelled", name="status_enum"))
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow)
 
-    owner = relationship('Customer', back_populates='work_orders')
-    
-    def __init__(self, id, customer_id, title, planned_date_begin, planned_date_end, status):
+    owner = relationship("Customer", back_populates="work_orders")
+
+    def __init__(
+        self, id, customer_id, title, planned_date_begin, planned_date_end, status
+    ):
         self.id = id
         self.customer_id = customer_id
         self.title = title
