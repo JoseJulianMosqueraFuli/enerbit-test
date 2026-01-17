d4ebf637744634a0e985155c3f76505e0da8f187
Jose Mosquera <jojumofu@gmail.com>
2026-01-17 16:24:38 -0500

    fixe(alembic): organize imports and formatting

--- alembic/versions/c2455710c8b1_.py
+++ alembic/versions/c2455710c8b1_.py
@@ -7,9 +7,9 @@ Create Date: 2023-10-27 17:57:54.984091
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = 'c2455710c8b1'
